d969563fe29ac7a81486494eb0b33e5df4a6dc71
lillycoder <ltheinh@gmail.com>
2019-06-27 19:15:26 +0000

    trouble shooting img_tag problem

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_23_212451) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_comments_on_place_id"
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
   end
 
