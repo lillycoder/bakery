f4b897c6457671f2ff0a722434dd2b6bb38aa5e4
lillycoder <ltheinh@gmail.com>
2019-06-29 18:09:30 +0000

    having problems in chapter 40 and 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,8 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@bakeryapp.com"
+  default from: 'no-reply@bakeryapp.com'
 
   def comment_added
-    mail(to: "luhrte@earthlink.net",
-      subject: "A comment has been added to your place")
+    mail(to: 'ltheinh@gmail.com',
+      subject: 'A comment has been added to your place')
   end
 end
